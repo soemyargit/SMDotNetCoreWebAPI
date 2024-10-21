30afa08d1d4901676450a0d7efaee16e3395c11a
Linn Thit Htoo <lth1212001@gmail.com>
2024-10-21 20:40:39 +0630

    [ Added ] QueryAsync

---
+++ SMDotNetCoreADO/Services/AdoDotNetService.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Data.SqlClient;
+using Newtonsoft.Json;
+using System.Data;
+
+namespace SMDotNetCoreADO.Services
+{
+    public class AdoDotNetService
+    {
+        private SqlConnection GetSqlConnection() => new("");
+
+        public async Task<List<T>> QueryAsync<T>(string query, SqlParameter[]? parameters = null, CommandType commandType = CommandType.Text)
+        {
+            try
+            {
+                SqlConnection connection = GetSqlConnection();
+                await connection.OpenAsync();
+
+                SqlCommand command = new(query, connection);
+                if (parameters is not null)
+                {
+                    command.Parameters.AddRange(parameters);
+                }
+
+                SqlDataAdapter adapter = new(command);
+                DataTable dt = new();
+                adapter.Fill(dt);
+                await connection.CloseAsync();
+
+                string jsonStr = JsonConvert.SerializeObject(dt);
+                var lst = JsonConvert.DeserializeObject<List<T>>(jsonStr)!;
+
+                return lst;
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+    }
+}
