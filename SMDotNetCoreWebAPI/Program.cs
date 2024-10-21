31d770b9c09ac87d9558fc409cce8cff66a194e1
Linn Thit Htoo <lth1212001@gmail.com>
2024-10-21 20:48:27 +0630

    -

--- SMDotNetCoreWebAPI/Program.cs
+++ SMDotNetCoreWebAPI/Program.cs
@@ -2,17 +2,13 @@ using SMDotNetCoreWebAPI.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddScoped<DapperService>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
