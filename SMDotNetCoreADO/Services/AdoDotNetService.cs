97ca02228dbde1c96cd20362efcb537305f32268
Linn Thit Htoo <lth1212001@gmail.com>
2024-10-21 20:42:10 +0630

    [ Added ] QueryFirstOrDefaultAsync

--- SMDotNetCoreADO/Services/AdoDotNetService.cs
+++ SMDotNetCoreADO/Services/AdoDotNetService.cs
@@ -15,7 +15,7 @@ namespace SMDotNetCoreADO.Services
                 SqlConnection connection = GetSqlConnection();
                 await connection.OpenAsync();
 
-                SqlCommand command = new(query, connection);
+                SqlCommand command = new(query, connection) { CommandType = commandType };
                 if (parameters is not null)
                 {
                     command.Parameters.AddRange(parameters);
@@ -30,6 +30,32 @@ namespace SMDotNetCoreADO.Services
                 var lst = JsonConvert.DeserializeObject<List<T>>(jsonStr)!;
 
                 return lst;
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+        public async Task<DataTable> QueryFirstOrDefaultAsync(string query, SqlParameter[]? parameters = null, CommandType commandType = CommandType.Text)
+        {
+            try
+            {
+                SqlConnection connection = GetSqlConnection();
+                await connection.OpenAsync();
+
+                SqlCommand command = new(query, connection) { CommandType = commandType };
+                if (parameters is not null)
+                {
+                    command.Parameters.AddRange(parameters);
+                }
+
+                SqlDataAdapter adapter = new(command);
+                DataTable dt = new();
+                adapter.Fill(dt);
+                await connection.CloseAsync();
+
+                return dt;
             }
             catch (Exception ex)
             {
