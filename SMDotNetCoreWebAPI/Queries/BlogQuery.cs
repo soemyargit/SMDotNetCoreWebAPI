511e1f20e8d723e3589954f314125a93f126ad44
Linn Thit Htoo <lth1212001@gmail.com>
2024-10-21 20:48:19 +0630

    -

--- SMDotNetCoreWebAPI/Queries/BlogQuery.cs
+++ SMDotNetCoreWebAPI/Queries/BlogQuery.cs
@@ -3,13 +3,9 @@
     public class BlogQuery
     {
         public static string GetBlogListQuery { get; } = @"SELECT BlogId, BlogTitle, BlogAuthor, BlogContent FROM Tbl_Blog";
-
         public static string GetBlogByIDQuery { get; } = @"SELECT BlogId, BlogTitle, BlogAuthor, BlogContent FROM Tbl_Blog WHERE BlogId = @BlogId";
-
         public static string CreateBlogQuery { get; } = @"INSERT INTO Tbl_Blog (BlogTitle, BlogAuthor, BlogContent) Values (@BlogTitle, @BlogAuthor, @BlogContent)";
-
         public static string UpdateBlogQuery { get; } = @"UPDATE Tbl_Blog SET BlogTitle = @BlogTitle, BlogAuthor = @BlogAuthor, BlogContent = @BlogContent WHERE BlogId = @BlogId";
         public static string DeleteBlogQuery { get; } = @"DELETE FROM Tbl_Blog WHERE BlogId = @BlogID";
-
     }
 }
