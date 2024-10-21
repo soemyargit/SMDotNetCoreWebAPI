0f16c9581034cb2502815d283eac8cd41d5f2849
Linn Thit Htoo <lth1212001@gmail.com>
2024-10-21 20:47:49 +0630

    -

--- SMDotNetCoreWebAPI/Services/DapperService.cs
+++ SMDotNetCoreWebAPI/Services/DapperService.cs
@@ -8,14 +8,14 @@ namespace SMDotNetCoreWebAPI.Services
 {
     public class DapperService
     {
-        
+
         #region Query Async
-        public async Task<List<T>> QueryAsync<T>(string query,object? parameters = null, CommandType commandType = CommandType.Text)
+        public async Task<List<T>> QueryAsync<T>(string query, object? parameters = null, CommandType commandType = CommandType.Text)
         {
             try
             {
                 using IDbConnection dbCon = GetSqlConnection();
-                var lst = await dbCon.QueryAsync<T>(query, parameters,commandType: commandType);
+                var lst = await dbCon.QueryAsync<T>(query, parameters, commandType: commandType);
                 return lst.ToList();
 
             }
@@ -26,23 +26,23 @@ namespace SMDotNetCoreWebAPI.Services
 
         #region Query First Or Default Async
 
-        public async Task<T?> QueryFirstOrDefaultAsync<T>(string query,object? parameters = null, CommandType commandType = CommandType.Text)
+        public async Task<T?> QueryFirstOrDefaultAsync<T>(string query, object? parameters = null, CommandType commandType = CommandType.Text)
         {
-            try 
-            { 
+            try
+            {
                 using IDbConnection dbConnection = GetSqlConnection();
-                var item = await dbConnection.QueryFirstOrDefaultAsync<T>(query, parameters, commandType: commandType); 
+                var item = await dbConnection.QueryFirstOrDefaultAsync<T>(query, parameters, commandType: commandType);
 
                 return item;
             }
-            catch(Exception ex) { throw; }  
+            catch (Exception ex) { throw; }
         }
 
         #endregion
 
         #region Execute Query Async
 
-        public async Task<int> ExecuteQueryAsync(string query,object parameters)
+        public async Task<int> ExecuteQueryAsync(string query, object parameters)
         {
             try
             {
@@ -50,7 +50,7 @@ namespace SMDotNetCoreWebAPI.Services
                 int resultInt = await dbConnection.ExecuteAsync(query, parameters);
                 return resultInt;
             }
-            catch(Exception ex) { throw; }
+            catch (Exception ex) { throw; }
         }
 
         #endregion
