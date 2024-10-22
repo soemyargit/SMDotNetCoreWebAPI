9022b8a18e52c98eeedb4c8061616964c783a1bf
Linn Thit Htoo <lth1212001@gmail.com>
2024-10-22 20:52:35 +0630

    -

--- SMDotNetCoreADO/Program.cs
+++ SMDotNetCoreADO/Program.cs
@@ -3,8 +3,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddScoped<ADOService>();
 
+builder.Services.AddScoped<ADOService>();
 builder.Services.AddScoped<AdoDotNetService>();
 
 var app = builder.Build();
