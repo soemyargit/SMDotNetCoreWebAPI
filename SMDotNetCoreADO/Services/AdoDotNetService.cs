dfa90b64c9d2e3af4f9d79a758c3a9bed455de7b
Linn Thit Htoo <lth1212001@gmail.com>
2024-10-22 20:51:34 +0630

    -

--- SMDotNetCoreADO/Services/AdoDotNetService.cs
+++ SMDotNetCoreADO/Services/AdoDotNetService.cs
@@ -2,86 +2,85 @@
 using Newtonsoft.Json;
 using System.Data;
 
-namespace SMDotNetCoreADO.Services
+namespace SMDotNetCoreADO.Services;
+
+public class AdoDotNetService
 {
-    public class AdoDotNetService
+    private SqlConnection GetSqlConnection() => new("");
+
+    public async Task<List<T>> QueryAsync<T>(string query, SqlParameter[]? parameters = null, CommandType commandType = CommandType.Text)
     {
-        private SqlConnection GetSqlConnection() => new("");
-
-        public async Task<List<T>> QueryAsync<T>(string query, SqlParameter[]? parameters = null, CommandType commandType = CommandType.Text)
+        try
         {
-            try
+            SqlConnection connection = GetSqlConnection();
+            await connection.OpenAsync();
+
+            SqlCommand command = new(query, connection) { CommandType = commandType };
+            if (parameters is not null)
             {
-                SqlConnection connection = GetSqlConnection();
-                await connection.OpenAsync();
-
-                SqlCommand command = new(query, connection) { CommandType = commandType };
-                if (parameters is not null)
-                {
-                    command.Parameters.AddRange(parameters);
-                }
-
-                SqlDataAdapter adapter = new(command);
-                DataTable dt = new();
-                adapter.Fill(dt);
-                await connection.CloseAsync();
-
-                string jsonStr = JsonConvert.SerializeObject(dt);
-                var lst = JsonConvert.DeserializeObject<List<T>>(jsonStr)!;
-
-                return lst;
-            }
-            catch (Exception ex)
-            {
-                throw;
-            }
-        }
-
-        public async Task<DataTable> QueryFirstOrDefaultAsync(string query, SqlParameter[]? parameters = null, CommandType commandType = CommandType.Text)
-        {
-            try
-            {
-                SqlConnection connection = GetSqlConnection();
-                await connection.OpenAsync();
-
-                SqlCommand command = new(query, connection) { CommandType = commandType };
-                if (parameters is not null)
-                {
-                    command.Parameters.AddRange(parameters);
-                }
-
-                SqlDataAdapter adapter = new(command);
-                DataTable dt = new();
-                adapter.Fill(dt);
-                await connection.CloseAsync();
-
-                return dt;
-            }
-            catch (Exception ex)
-            {
-                throw;
-            }
-        }
-
-        public async Task<int> ExecuteAsync(string query, SqlParameter[] parameters)
-        {
-            try
-            {
-                SqlConnection connection = GetSqlConnection();
-                await connection.OpenAsync();
-
-                SqlCommand command = new(query, connection);
                 command.Parameters.AddRange(parameters);
-
-                int result = await command.ExecuteNonQueryAsync();
-                await connection.CloseAsync();
-
-                return result;
             }
-            catch (Exception ex)
+
+            SqlDataAdapter adapter = new(command);
+            DataTable dt = new();
+            adapter.Fill(dt);
+            await connection.CloseAsync();
+
+            string jsonStr = JsonConvert.SerializeObject(dt);
+            var lst = JsonConvert.DeserializeObject<List<T>>(jsonStr)!;
+
+            return lst;
+        }
+        catch (Exception ex)
+        {
+            throw;
+        }
+    }
+
+    public async Task<DataTable> QueryFirstOrDefaultAsync(string query, SqlParameter[]? parameters = null, CommandType commandType = CommandType.Text)
+    {
+        try
+        {
+            SqlConnection connection = GetSqlConnection();
+            await connection.OpenAsync();
+
+            SqlCommand command = new(query, connection) { CommandType = commandType };
+            if (parameters is not null)
             {
-                throw;
+                command.Parameters.AddRange(parameters);
             }
+
+            SqlDataAdapter adapter = new(command);
+            DataTable dt = new();
+            adapter.Fill(dt);
+            await connection.CloseAsync();
+
+            return dt;
+        }
+        catch (Exception ex)
+        {
+            throw;
+        }
+    }
+
+    public async Task<int> ExecuteAsync(string query, SqlParameter[] parameters)
+    {
+        try
+        {
+            SqlConnection connection = GetSqlConnection();
+            await connection.OpenAsync();
+
+            SqlCommand command = new(query, connection);
+            command.Parameters.AddRange(parameters);
+
+            int result = await command.ExecuteNonQueryAsync();
+            await connection.CloseAsync();
+
+            return result;
+        }
+        catch (Exception ex)
+        {
+            throw;
         }
     }
 }
