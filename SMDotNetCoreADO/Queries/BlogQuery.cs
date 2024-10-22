46177a888584e229621dd35cf2326272af434315
Linn Thit Htoo <lth1212001@gmail.com>
2024-10-22 20:51:23 +0630

    -

--- SMDotNetCoreADO/Queries/BlogQuery.cs
+++ SMDotNetCoreADO/Queries/BlogQuery.cs
@@ -1,11 +1,10 @@
-﻿namespace SMDotNetCoreADO.Queries
+﻿namespace SMDotNetCoreADO.Queries;
+
+public class BlogQuery ()
 {
-    public class BlogQuery ()
-    {
-        public static string GetBlogListQuery { get; } = @"SELECT BlogId, BlogTitle, BlogAuthor, BlogContent FROM Tbl_Blog";
-        public static string GetBlogByIDQuery { get; } = @"SELECT BlogId, BlogTitle, BlogAuthor, BlogContent FROM Tbl_Blog WHERE BlogId = @BlogId";
-        public static string CreateBlogQuery { get; } = @"INSERT INTO Tbl_Blog (BlogTitle, BlogAuthor, BlogContent) Values (@BlogTitle, @BlogAuthor, @BlogContent)";
-        public static string UpdateBlogQuery { get; } = @"UPDATE Tbl_Blog SET BlogTitle = @BlogTitle, BlogAuthor = @BlogAuthor, BlogContent = @BlogContent WHERE BlogId = @BlogId";
-        public static string DeleteBlogQuery { get; } = @"DELETE FROM Tbl_Blog WHERE BlogId = @BlogID";
-    }
+    public static string GetBlogListQuery { get; } = @"SELECT BlogId, BlogTitle, BlogAuthor, BlogContent FROM Tbl_Blog";
+    public static string GetBlogByIDQuery { get; } = @"SELECT BlogId, BlogTitle, BlogAuthor, BlogContent FROM Tbl_Blog WHERE BlogId = @BlogId";
+    public static string CreateBlogQuery { get; } = @"INSERT INTO Tbl_Blog (BlogTitle, BlogAuthor, BlogContent) Values (@BlogTitle, @BlogAuthor, @BlogContent)";
+    public static string UpdateBlogQuery { get; } = @"UPDATE Tbl_Blog SET BlogTitle = @BlogTitle, BlogAuthor = @BlogAuthor, BlogContent = @BlogContent WHERE BlogId = @BlogId";
+    public static string DeleteBlogQuery { get; } = @"DELETE FROM Tbl_Blog WHERE BlogId = @BlogID";
 }
